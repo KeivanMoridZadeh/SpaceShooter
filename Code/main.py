2a72673df486c2533fd68e469313607a5502e1df
kayva <kayvanmoridzadeh@gmail.com>
2025-01-11 04:14:38 -0500

    Initial commit

---
+++ Code/main.py
@@ -0,0 +1,182 @@
+import pygame
+from os.path import join
+from random import randint, uniform
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        
+        self.image = pygame.image.load(join('images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_frect(center=(WIDTH // 2, HEIGHT // 2))
+        self.direction = pygame.math.Vector2()
+        self.speed = 300
+        
+        # Cooldown
+        self.can_shoot = True
+        self.liser_shoot_time = 0
+        self.cooldown_duration = 400
+
+        # Mask
+        self.mask = pygame.mask.from_surface(self.image)
+        
+    def laset_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.laser_shoot_time >= self.cooldown_duration:
+                self.can_shoot = True
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        
+        # Normalize direction to maintain consistent speed
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt
+        
+        recent_keys = pygame.key.get_just_pressed()
+        if recent_keys[pygame.K_SPACE] and self.can_shoot:
+            Laser(laser_surf, self.rect.midtop, (all_sprites, laser_sprites))
+            self.can_shoot = False
+            self.laser_shoot_time = pygame.time.get_ticks()
+            
+        self.laset_timer()
+
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups, surf):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center=(randint(0, WIDTH), randint(0, HEIGHT)))
+
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(midbottom=pos)
+
+    def update(self, dt):
+        self.rect.centery -= 400 * dt
+        if self.rect.bottom < 0:
+            self.kill()
+
+
+class Metor(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, groups):
+        super().__init__(groups)
+        self.original_surf = surf
+        self.image = surf
+        self.rect = self.image.get_frect(center=pos)
+        self.start_time = pygame.time.get_ticks()
+        self.lifetime = 3000
+        self.direction = pygame.Vector2(uniform(-0.5, 0.5), 1)
+        self.speed = randint(400, 500)
+        self.rotation_speed = randint(40, 100)
+        self.rotation = 0
+
+    def update(self, dt):
+        self.rect.center += self.direction * self.speed * dt
+        if pygame.time.get_ticks() - self.start_time >= self.lifetime:
+            self.kill()
+        self.rotation += self.rotation_speed * dt
+        self.image = pygame.transform.rotozoom(self.original_surf, self.rotation, 1)
+        self.rect = self.image.get_frect(center=self.rect.center)
+
+
+class AnimatedExplosion(pygame.sprite.Sprite):
+    def __init__(self, frames, pos, groups):
+        super().__init__(groups)
+        self.frames = frames
+        self.frame_index = 0
+        self.image = frames[self.frame_index]
+        self.rect = self.image.get_frect(center=pos)
+    
+    def update(self, dt):
+        self.frame_index += 25 * dt
+        if self.frame_index < len(self.frames):
+            self.image = self.frames[int(self.frame_index)]
+        else:
+            self.kill()
+
+
+def collision():
+    global running
+    collision_sprite = pygame.sprite.spritecollide(player, metor_sprite, True, pygame.sprite.collide_mask)
+    if collision_sprite:
+        running = False
+    for laser in laser_sprites:
+        collided_sprites = pygame.sprite.spritecollide(laser, metor_sprite, True)
+        if collided_sprites:
+            laser.kill()
+            AnimatedExplosion(explosion_frames, laser.rect.midtop, all_sprites)
+
+
+def display_score():
+    current_time = pygame.time.get_ticks() // 100
+    text_surf = font.render(str(current_time), True, (240, 240, 240))
+    text_rect = text_surf.get_frect(midbottom=(WIDTH / 2, HEIGHT - 50))
+    
+    display_surface.blit(text_surf, text_rect)
+    pygame.draw.rect(display_surface, (240, 240, 240), text_rect.inflate(20, 10).move(0, -8), 5, 10)
+
+
+# General setup
+pygame.init()
+WIDTH, HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+running = True
+pygame.display.set_caption("Space Shooter")
+clock = pygame.time.Clock()
+
+# Import assets
+star_surf = pygame.image.load(join("images", "star.png")).convert_alpha()
+meteor_surf = pygame.image.load(join("images", "meteor.png")).convert_alpha()
+laser_surf = pygame.image.load(join("images", "laser.png")).convert_alpha()
+font = pygame.font.Font(join("images", "Oxanium-Bold.ttf"), 40)
+explosion_frames = [pygame.image.load(join('images', 'explosion', f'{i}.png')).convert_alpha() for i in range(21)]
+
+# Sprite groups
+all_sprites = pygame.sprite.Group()
+metor_sprite = pygame.sprite.Group()
+laser_sprites = pygame.sprite.Group()
+
+for i in range(20):
+    Star(all_sprites, star_surf)
+
+player = Player(all_sprites)
+
+# Import path
+path = join('images', 'player.png')
+
+# Custom event -> metor event
+metor_event = pygame.event.custom_type()
+pygame.time.set_timer(metor_event, 500)
+
+# Game loop
+while running:
+    dt = clock.tick() / 1000
+    
+    # Event Loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == metor_event:
+            x, y = randint(0, WIDTH), randint(-200, -100)
+            Metor(meteor_surf, (x, y), (all_sprites, metor_sprite))
+
+    all_sprites.update(dt)
+    
+    # Draw the game
+    display_surface.fill("#3a2e3f")
+    all_sprites.draw(display_surface)
+    
+    # Update the entire window
+    collision()
+    display_score()
+
+    pygame.display.update()
+
+pygame.quit()
